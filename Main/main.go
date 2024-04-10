5ce27ec7ef1725cb2a167a3a223d5db21f18c8c8
cedric <cedric.buriand@ynov.com>
2024-04-10 11:20:04 +0200

    Feat: add server in main.go

--- Main/main.go
+++ Main/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./html/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, nil)
+}
+
 func main() {
-	println("Hello, World!")
+	http.HandleFunc("/", Home)
+
+	fs := http.FileServer(http.Dir("./server/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.ListenAndServe(":8080", nil)
 }
