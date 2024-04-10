a1d32dad603bcfdd3d877b45dc42037fba249d56
HoDoH-H <144995071+HoDoH-H@users.noreply.github.com>
2024-04-10 11:33:48 +0200

    Add account manager

---
+++ Go/accountManager.go
@@ -0,0 +1,12 @@
+package accountManager
+
+type User struct {
+	Username string
+	Password string
+	Email    string
+	id       int
+}
+
+func addNewAccount(newUser User) {
+	// Add new user to database
+}
