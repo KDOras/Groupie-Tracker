3b282a0516c340422bf461ddff21e2a8916e40f8
Dany Boulanger <144791870+KDShadowDragon@users.noreply.github.com>
2024-04-26 11:47:04 +0200

    Add Link between Html and Login/Register

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"groupie/src/databaseManager"
 	"html/template"
 	"log"
@@ -41,14 +43,39 @@ func Login(w http.ResponseWriter, r *http.Request, LoginVar databaseManager.User
 	template.Execute(w, nil)
 }
 
+func Action(w http.ResponseWriter, r *http.Request, db *sql.DB, user *databaseManager.ConnectedUser) {
+	r.ParseForm()
+	fmt.Println("TA MERE AXEL")
+	if r.Method == "post" {
+		if r.URL.Path == "/Register" {
+			user := databaseManager.User{
+				Username: r.FormValue("username"),
+				Password: r.FormValue("password"),
+				Email:    r.FormValue("email"),
+			}
+			databaseManager.CreateNewUser(db, user)
+		} else if r.URL.Path == "/Login" {
+			inputUsername := r.FormValue("username")
+			inputPassword := r.FormValue("password")
+			user, _ = databaseManager.LoggingIn(db, inputUsername, inputPassword)
+			fmt.Println(user.Username)
+		}
+	}
+	http.Redirect(w, r, "/Gamepage", http.StatusSeeOther)
+}
+
 func main() {
+	user := databaseManager.ConnectedUser{}
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/Gamepage", GamePage)
 	http.HandleFunc("/Register", func(w http.ResponseWriter, r *http.Request) {
 		Create(w, r, databaseManager.User{})
 	})
 	http.HandleFunc("/Login", func(w http.ResponseWriter, r *http.Request) {
-		Create(w, r, databaseManager.User{})
+		Login(w, r, databaseManager.User{})
+	})
+	http.HandleFunc("/Action", func(w http.ResponseWriter, r *http.Request) {
+		Action(w, r, databaseManager.InitDatabase("SQL/database.db"), &user)
 	})
 	fs := http.FileServer(http.Dir("./server/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
